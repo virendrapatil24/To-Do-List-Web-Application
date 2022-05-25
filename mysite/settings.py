47a99c0f1c83287b3757deee3fae7ca8cb91348d
Virendra <virendrapatil.24vp@gmail.com>
2022-05-25 19:47:11 +0530

    added changes for deployment on heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=u^)3i1zly9lg#4b_zb_vicg4&3@5fc95*cwz(q219#*4d*31n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 from django.urls import include, path
 from register import views as v
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('', include('main.urls')), # main will be the name of your app
     path("register/", v.register, name="register"),
     path('', include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
